ba6d075d11d64dc4f65de68cf7dd04392c8775bd
Ching Chow <ching.chow.sc@gmail.com>
2018-07-02 12:33:04 -0400

    Update to Python 3.7

--- hb/cli.py
+++ hb/cli.py
@@ -6,68 +6,89 @@ from typing import List
 
 import click
 
-import main as hb
+from main import Checksum
 
 
-@click.group(context_settings=dict(help_option_names=["-h", "--help"]))
+@click.group(context_settings={"help_option_names": ["-h", "--help"]})
 @click.version_option(version="0.1.0")
 def cli() -> None:
     """Hash Brown: Compute and verify hashes."""
     pass
 
 @cli.command()  # type: ignore
-@click.option("-a", "--algorithm", required=True, type=click.Choice(hb.HASHLIBS + hb.ZLIBS))
+@click.option("-a", "--algorithm", required=True, type=click.Choice(Checksum.supported()))
 @click.option("-p", "--progress", is_flag=True, help="Display hashing progress.")
-@click.argument("paths", required=True, nargs=-1, type=click.Path())
+@click.argument("paths", required=True, nargs=-1)
 def compute(algorithm: str, paths: List[str], progress: bool) -> None:
     """Compute a hash."""
     count = 0
     for path in paths:
         for filename in iglob(path, recursive=True):
             if isfile(filename):
-                click.echo(f"{algorithm} ({filename}) = {hb.compute(algorithm, filename, show_progress=progress)}")
+                checksum = Checksum(filename, show_progress=progress)
+                if algorithm == "blake2b":
+                    result = checksum.blake2b()
+                elif algorithm == "blake2s":
+                    result = checksum.blake2s()
+                elif algorithm == "md5":
+                    result = checksum.md5()
+                elif algorithm == "sha1":
+                    result = checksum.sha1()
+                elif algorithm == "sha224":
+                    result = checksum.sha224()
+                elif algorithm == "sha256":
+                    result = checksum.sha256()
+                elif algorithm == "sha384":
+                    result = checksum.sha384()
+                elif algorithm == "sha512":
+                    result = checksum.sha512()
+                elif algorithm == "adler32":
+                    result = checksum.adler32()
+                else:
+                    result = checksum.crc32()
+                click.echo(f"{algorithm} ({filename}) = {result}")
                 count += 1
         if not count:
             click.echo(f"Error: No files matched the pattern: '{path}'")
         count = 0
 
-@cli.command()  # type: ignore
-@click.option("-a", "--algorithm", required=True, type=click.Choice(hb.HASHLIBS + hb.ZLIBS))
-@click.option("-p", "--progress", is_flag=True, help="Display hashing progress.")
-@click.argument("path", required=True, type=click.Path(exists=True, dir_okay=False))
-@click.argument("digest", required=True)
-def single(algorithm: str, path: str, digest: str, progress: bool) -> None:
-    """Verify a single hash on the command line."""
-    click.echo(f"{algorithm} ({path}) = {digest} {'OK' if digest == hb.compute(algorithm, path, show_progress=progress) else 'BAD'}")
+# @cli.command()  # type: ignore
+# @click.option("-a", "--algorithm", required=True, type=click.Choice(hb.HASHLIBS + hb.ZLIBS))
+# @click.option("-p", "--progress", is_flag=True, help="Display hashing progress.")
+# @click.argument("path", required=True, type=click.Path(exists=True, dir_okay=False))
+# @click.argument("digest", required=True)
+# def single(algorithm: str, path: str, digest: str, progress: bool) -> None:
+#     """Verify a single hash on the command line."""
+#     click.echo(f"{algorithm} ({path}) = {digest} {'OK' if digest == hb.compute(algorithm, path, show_progress=progress) else 'BAD'}")
 
-@cli.command()  # type: ignore
-@click.option("-p", "--progress", is_flag=True, help="Display hashing progress.")
-@click.argument("path", required=True, type=click.Path(exists=True, dir_okay=False))
-def verify(path: str, progress: bool) -> None:
-    """Verify multiple hashes with a checksum file."""
-    good = 0
-    bad = 0
-    skip = 0
-    for algorithm, filepath, digest in hb.parse(path):
-        output = f"{algorithm} ({filepath}) = {digest} "
-        try:
-            computed = hb.compute(algorithm, filepath, show_progress=progress)
-        except FileNotFoundError:
-            click.echo(output + "SKIP: File not found")
-            skip += 1
-        except OSError:
-            click.echo(output + "SKIP: Unable to read file")
-            skip += 1
-        except ValueError as err:
-            click.echo(output + f"SKIP: {err}")
-            skip += 1
-        else:
-            if digest == computed:
-                good += 1
-            else:
-                click.echo(output + "BAD")
-                bad += 1
-    click.echo(f"GOOD: {good}, BAD: {bad}, SKIP: {skip}")
+# @cli.command()  # type: ignore
+# @click.option("-p", "--progress", is_flag=True, help="Display hashing progress.")
+# @click.argument("path", required=True, type=click.Path(exists=True, dir_okay=False))
+# def verify(path: str, progress: bool) -> None:
+#     """Verify multiple hashes with a checksum file."""
+#     good = 0
+#     bad = 0
+#     skip = 0
+#     for algorithm, filepath, digest in hb.parse(path):
+#         output = f"{algorithm} ({filepath}) = {digest} "
+#         try:
+#             computed = hb.compute(algorithm, filepath, show_progress=progress)
+#         except FileNotFoundError:
+#             click.echo(output + "SKIP: File not found")
+#             skip += 1
+#         except OSError:
+#             click.echo(output + "SKIP: Unable to read file")
+#             skip += 1
+#         except ValueError as err:
+#             click.echo(output + f"SKIP: {err}")
+#             skip += 1
+#         else:
+#             if digest == computed:
+#                 good += 1
+#             else:
+#                 click.echo(output + "BAD")
+#                 bad += 1
+#     click.echo(f"GOOD: {good}, BAD: {bad}, SKIP: {skip}")
 
 if __name__ == "__main__":
     cli()
