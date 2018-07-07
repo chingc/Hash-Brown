34f756883f90fcd43f407ab5a759534e446fa053
Ching Chow <ching.chow.sc@gmail.com>
2018-07-06 21:33:41 -0400

    Update tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,55 @@
 """Fixtures"""
 
 from pathlib import Path
-from typing import Callable
+from typing import List
 
 import pytest
 
+from hb.main import Checksum
+
+
+_FOX = str(Path(__file__).parent / "test_files" / "fox.txt")
 
 @pytest.fixture  # type: ignore
-def get_file() -> Callable[[str], str]:
-    return lambda x: str(Path(__file__).parent / "test_files" / x)
+def supported() -> List[str]:
+    return Checksum.supported()
+
+@pytest.fixture  # type: ignore
+def blake2b() -> str:
+    return Checksum(_FOX).blake2b()
+
+@pytest.fixture  # type: ignore
+def blake2s() -> str:
+    return Checksum(_FOX).blake2s()
+
+@pytest.fixture  # type: ignore
+def md5() -> str:
+    return Checksum(_FOX).md5()
+
+@pytest.fixture  # type: ignore
+def sha1() -> str:
+    return Checksum(_FOX).sha1()
+
+@pytest.fixture  # type: ignore
+def sha224() -> str:
+    return Checksum(_FOX).sha224()
+
+@pytest.fixture  # type: ignore
+def sha256() -> str:
+    return Checksum(_FOX).sha256()
+
+@pytest.fixture  # type: ignore
+def sha384() -> str:
+    return Checksum(_FOX).sha384()
+
+@pytest.fixture  # type: ignore
+def sha512() -> str:
+    return Checksum(_FOX).sha512()
+
+@pytest.fixture  # type: ignore
+def adler32() -> str:
+    return Checksum(_FOX).adler32()
+
+@pytest.fixture  # type: ignore
+def crc32() -> str:
+    return Checksum(_FOX).crc32()
--- tests/test_hb.py
+++ tests/test_hb.py
@@ -1,35 +1,46 @@
 """Tests"""
 
-from typing import Callable
+from typing import List
 
-import pytest
+# def test_parse(get_file: Callable[[str], str]) -> None:
+#     for algo, path, digest in parse(get_file("checksum_good.txt")):
+#         assert algo == "md5"
+#         assert path == "test.txt"
+#         assert digest == "c08420f4c716f3814c268dd845356276"
 
-from hb.adapter import Adapter
-from hb.main import compute, parse
+#     for bad in [1, 2, 3, 4]:
+#         with pytest.raises(ValueError):
+#             for algo, path, digest in parse(get_file(f"checksum_bad_0{bad}.txt")): pass
 
+def test_supported(supported: List[str]) -> None:
+    assert supported == ["blake2b", "blake2s", "md5", "sha1", "sha224", "sha256", "sha384", "sha512", "adler32", "crc32"]
 
-def test_adapter(get_file: Callable[[str], str]) -> None:
-    assert compute("adler32", get_file("0ada12d6.txt")) == "0ada12d6"
-    assert compute("crc32", get_file("4a2e4fa3.txt")) == "4a2e4fa3"
+def test_blake2b(blake2b: str) -> None:
+    assert blake2b == "1e5bbd57cc8fcdea22e4155f43672f3f3e655295c21a15405d1844b4be90cced030064a57e231b989b85db4a8a9e47accbd60be95608e52203da27b7cb18c85e"
 
-    with pytest.raises(ValueError):
-        Adapter("nonexistent")
+def test_blake2s(blake2s: str) -> None:
+    assert blake2s == "462b84b6ed0c29b302c4f0207f2b344a8c1ade6b5d5ff8a326fc681681a35899"
 
-def test_compute(get_file: Callable[[str], str]) -> None:
-    assert compute("md5", get_file("a97e741e7b0a63dfb156fa8bf372736c.txt"), True) == "a97e741e7b0a63dfb156fa8bf372736c"
+def test_md5(md5: str) -> None:
+    assert md5 == "f2789658fd0737100f4dd1fac58c08e9"
 
-    with pytest.raises(ValueError):
-        compute("nonexistent", "")
+def test_sha1(sha1: str) -> None:
+    assert sha1 == "5aaf0a05ace3134ac1dc48004c0eacd9003eff5d"
 
-    with pytest.raises(FileNotFoundError):
-        compute("md5", "")
+def test_sha224(sha224: str) -> None:
+    assert sha224 == "6338d3bc74dbbb5048cc2511db3bb639d4666d431574ca1c47ba021d"
 
-def test_parse(get_file: Callable[[str], str]) -> None:
-    for algo, path, digest in parse(get_file("checksum_good.txt")):
-        assert algo == "md5"
-        assert path == "test.txt"
-        assert digest == "c08420f4c716f3814c268dd845356276"
+def test_sha256(sha256: str) -> None:
+    assert sha256 == "22f54567ed79dcf2423e7202a02482ce1406e42152e158ba8e0f44c6bbc6eaf9"
 
-    for bad in [1, 2, 3, 4]:
-        with pytest.raises(ValueError):
-            for algo, path, digest in parse(get_file(f"checksum_bad_0{bad}.txt")): pass
+def test_sha384(sha384: str) -> None:
+    assert sha384 == "de3c836b44e17e878ee3461a6d3d86abd037b6b9cc806e66ca30b5a14302bce48668dc2b68bc3a2f2b0fdb52d91a7483"
+
+def test_sha512(sha512: str) -> None:
+    assert sha512 == "b62a16f71f55b7baf52a4074238f85dc9543eb11a04b8d568fac299a9aec36f3b63b9b03b2101a5988ed51f8080d17f55726a1a06b5330040c94f4fa455f13a7"
+
+def test_adler32(adler32: str) -> None:
+    assert adler32 == "7bb40ff1"
+
+def test_crc32(crc32: str) -> None:
+    assert crc32 == "55f9bb0b"
