5afb46307fcda5bdfa093f5fc9aed06424ef3015
Ching Chow <ching.chow.sc@gmail.com>
2018-07-09 01:37:20 -0400

    Implement checklist

--- hb/cli.py
+++ hb/cli.py
@@ -8,6 +8,11 @@ import click
 from main import Checksum
 
 
+def _compare(actual: str, expected: str):
+    if actual.endswith(expected):
+        return click.style("OK", fg="green")
+    return click.style(f"ACTUAL: {expected}", fg="red")
+
 @click.version_option(version="0.1.0")
 @click.command(context_settings={"help_option_names": ["-h", "--help"]})
 @click.option("-a", "--algorithm", type=click.Choice(Checksum.supported.split(" ")))
@@ -22,44 +27,29 @@ def cli(algorithm: str, check: str, match: str, file: str) -> None:
             if isfile(filename):
                 output = Checksum(filename).print(algorithm)
                 if match:
-                    output += f" {click.style('OK', fg='green') if output.endswith(match) else click.style('BAD', fg='red')}"
+                    output = f"{algorithm} ({filename}) = {match}"
+                    output += f" {_compare(output, match)}"
                 click.echo(output)
                 computed += 1
         if not computed:
             click.echo(f"No files matched the pattern: '{file}'")
     elif check:
-        pass
+        try:
+            for algorithm, path, checksum in Checksum.parse(file):
+                output = f"{algorithm} ({path}) = {checksum}"
+                try:
+                    checksum = Checksum(path).compute(algorithm)
+                except FileNotFoundError:
+                    output += f" {click.style('SKIP: File not found', fg='yellow')}"
+                except OSError:
+                    output += f" {click.style('SKIP: Unable to read', fg='yellow')}"
+                else:
+                    output += f" {_compare(output, checksum)}"
+                click.echo(output)
+        except (OSError, ValueError) as error:
+            click.echo(error)
     else:
         pass
-
-# @cli.command()  # type: ignore
-# @click.option("-p", "--progress", is_flag=True, help="Display hashing progress.")
-# @click.argument("path", required=True, type=click.Path(exists=True, dir_okay=False))
-# def verify(path: str, progress: bool) -> None:
-#     """Verify multiple hashes with a checksum file."""
-#     good = 0
-#     bad = 0
-#     skip = 0
-#     for algorithm, filepath, digest in hb.parse(path):
-#         output = f"{algorithm} ({filepath}) = {digest} "
-#         try:
-#             computed = hb.compute(algorithm, filepath, show_progress=progress)
-#         except FileNotFoundError:
-#             click.echo(output + "SKIP: File not found")
-#             skip += 1
-#         except OSError:
-#             click.echo(output + "SKIP: Unable to read file")
-#             skip += 1
-#         except ValueError as err:
-#             click.echo(output + f"SKIP: {err}")
-#             skip += 1
-#         else:
-#             if digest == computed:
-#                 good += 1
-#             else:
-#                 click.echo(output + "BAD")
-#                 bad += 1
-#     click.echo(f"GOOD: {good}, BAD: {bad}, SKIP: {skip}")
 
 if __name__ == "__main__":
     cli()
