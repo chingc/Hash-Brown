d52baa7d49d3cc25407c5008bc723c747dd7900d
Ching Chow <ching.chow.sc@gmail.com>
2019-02-10 11:46:30 -0500

    Remove typing from tests

--- hb/tests/conftest.py
+++ hb/tests/conftest.py
@@ -2,7 +2,6 @@
 
 from hashlib import algorithms_guaranteed
 from pathlib import Path
-from typing import List
 
 from pytest import fixture
 
@@ -14,36 +13,36 @@ FIXTURES = CWD / "fixtures"
 
 
 @fixture
-def supported() -> List[str]:
+def supported():
     """Guaranteed algorithms from hashlib minus ones with underscore plus adler32 and crc32."""
     return [x for x in sorted(algorithms_guaranteed) if "_" not in x] + ["adler32", "crc32"]
 
 @fixture
-def version() -> str:
+def version():
     """Version from the pyproject.toml file."""
     ver = "Cannot find version!"
-    with open(CWD.parent / "pyproject.toml", "r") as lines:
-        for line in lines:
+    with open(CWD.parent / "pyproject.toml", "r") as stream:
+        for line in stream:
             if line.startswith("version = "):
                 ver = line.strip().split(" = ")[1][1:-1]
     return ver
 
 @fixture
-def good_checklists() -> List[Path]:
+def good_checklists():
     """All good checklists."""
     return [FIXTURES / f"checklist_good_0{i}.txt" for i in range(1, 2)]
 
 @fixture
-def bad_checklists() -> List[Path]:
+def bad_checklists():
     """All bad checklists."""
     return [FIXTURES / f"checklist_bad_0{i}.txt" for i in range(1, 4)]
 
 @fixture
-def fox() -> Checksum:
+def fox():
     """Checksum object."""
     return Checksum(FIXTURES / "fox.txt")
 
 @fixture
-def fox_progress() -> Checksum:
+def fox_progress():
     """Checksum object with zero threshold."""
     return Checksum(FIXTURES / "fox.txt", threshold=0)
--- hb/tests/test_main.py
+++ hb/tests/test_main.py
@@ -1,38 +1,36 @@
 """Tests"""
 
-from typing import List
-
 from pytest import raises
 
 from hb.main import Checksum
 
 
-def test_supported(supported: List[str]) -> None:
+def test_supported(supported):
     assert Checksum.SUPPORTED == tuple(supported)
 
-def test_unsupported() -> None:
+def test_unsupported():
     with raises(ValueError):
         Checksum(".").compute("unsupported")
 
-# def test_version(version: str) -> None:
+# def test_version(version):
 #     assert Checksum.VERSION == version
 
-def test_parse_success(good_checklists: List[str]) -> None:
+def test_parse_success(good_checklists):
     for checklist in good_checklists:
         for algorithm, path, checksum in Checksum.parse(checklist):
             assert algorithm == "md5"
             assert path == "test.txt"
             assert checksum == "c08420f4c716f3814c268dd845356276"
 
-def test_parse_failure(bad_checklists: List[str]) -> None:
+def test_parse_failure(bad_checklists):
     for checklist in bad_checklists:
         with raises(ValueError):
             Checksum.parse(checklist)
 
-def test_print() -> None:
+def test_print():
     assert Checksum.print("a", "b", "c") == "a (b) = c"
 
-def test_path_property(fox: Checksum) -> None:
+def test_path_property(fox):
     assert fox.md5 == "0d7006cd055e94cf614587e1d2ae0c8e"
     assert fox.path != "abc"
     assert "md5" in fox.checksums
@@ -41,37 +39,37 @@ def test_path_property(fox: Checksum) -> None:
     assert fox.path == "abc"
     assert not fox.checksums
 
-def test_blake2b(fox: Checksum) -> None:
+def test_blake2b(fox):
     assert fox.blake2b == "91b27f225ee86f26ef2103de210fd19e7e9f6cb3d10f204a6ad359d90abbd5f06425dc9dc801a035d86d6dff977a69b5922a2d22a143ed8d63f026bb875009ec"
 
-def test_blake2s(fox: Checksum) -> None:
+def test_blake2s(fox):
     assert fox.blake2s == "8c580d37bdb44575570c971f29b4e8fa25346a105b959880260cdb6946f4d983"
 
-def test_md5(fox: Checksum) -> None:
+def test_md5(fox):
     assert fox.md5 == "0d7006cd055e94cf614587e1d2ae0c8e"
 
-def test_sha1(fox: Checksum) -> None:
+def test_sha1(fox):
     assert fox.sha1 == "9c04cd6372077e9b11f70ca111c9807dc7137e4b"
 
-def test_sha224(fox: Checksum) -> None:
+def test_sha224(fox):
     assert fox.sha224 == "e88799b0d0d5becc6791837fa95388d4056f1250a511d14829766663"
 
-def test_sha256(fox: Checksum) -> None:
+def test_sha256(fox):
     assert fox.sha256 == "b47cc0f104b62d4c7c30bcd68fd8e67613e287dc4ad8c310ef10cbadea9c4380"
 
-def test_sha384(fox: Checksum) -> None:
+def test_sha384(fox):
     assert fox.sha384 == "d51d28d0141e56f692952ea14861898e2b417b922831e0f4bcdbc326a7fe1e9d9563182e83d3a8af66f68536e0d42b88"
 
-def test_sha512(fox: Checksum) -> None:
+def test_sha512(fox):
     assert fox.sha512 == "020da0f4d8a4c8bfbc98274027740061d7df52ee07091ed6595a083e0f45327bbe59424312d86f218b74ed2e25507abaf5c7a5fcf4cafcf9538b705808fd55ec"
 
-def test_adler32(fox: Checksum) -> None:
+def test_adler32(fox):
     assert fox.adler32 == "7bf61012"
 
-def test_crc32(fox: Checksum) -> None:
+def test_crc32(fox):
     assert fox.crc32 == "eb50cc6a"
 
-def test_memoization(mocker, fox: Checksum) -> None:
+def test_memoization(mocker, fox):
     mocker.spy(fox, 'get')
     mocker.spy(fox, 'compute')
 
