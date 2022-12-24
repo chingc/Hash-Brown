4f9cb7b1d5d893cce8735bf3263fc9396838a788
Ching Chow <ching@getbento.com>
2022-12-23 22:48:48 -0500

    generate random file for compute

--- tests/test_hb.py
+++ tests/test_hb.py
@@ -1,15 +1,21 @@
 """test_hb.py"""
 
 # mypy: ignore-errors
+# pylint: disable=W
 
 import hashlib
+import os
+import tempfile
+import uuid
 import zlib
+
+import pytest
 
 from hb import hb
 from hb.hb import HashBrown
 
 
-def use_stdlib(algo, path) -> str:
+def use_stdlib(algo, path):
     """Compute the hexdigest using hashlib and zlib."""
     match algo:
         case "adler32":
@@ -36,18 +42,30 @@ def use_stdlib(algo, path) -> str:
     return hexdigest
 
 
-def test_compute(subtests):
+@pytest.fixture
+def randfile():
+    """Generate a random ~1MB file."""
+    temp = tempfile.NamedTemporaryFile(mode="w", delete=False, encoding="utf-8")
+    print("\n".join([str(uuid.uuid4()) for _ in range(30000)]), file=temp)
+    temp.close()
+    assert os.path.getsize(temp.name) > 1000000
+    yield temp.name
+    os.unlink(temp.name)
+    assert not os.path.exists(temp.name)
+
+
+def test_compute(subtests, randfile):
     """Compare with direct usage of hashlib and zlib."""
     for algo in hashlib.algorithms_guaranteed | {"adler32", "crc32"}:
         with subtests.test(algo=algo):
-            using_hb = HashBrown(algo, "LICENSE").compute()
-            using_stdlib = use_stdlib(algo, "LICENSE")
+            using_hb = HashBrown(algo, randfile).compute()
+            using_stdlib = use_stdlib(algo, randfile)
             assert using_hb == using_stdlib
 
 
-def test_pprint(subtests):
+def test_pprint(subtests, randfile):
     """Check pretty print output."""
     for algo in hashlib.algorithms_guaranteed | {"adler32", "crc32"}:
         with subtests.test(algo=algo):
-            hexdigest = use_stdlib(algo, "LICENSE")
-            assert hb.compute(algo, "LICENSE") == f"{hexdigest} {algo} LICENSE"
+            hexdigest = use_stdlib(algo, randfile)
+            assert hb.compute(algo, randfile) == f"{hexdigest} {algo} {randfile}"
