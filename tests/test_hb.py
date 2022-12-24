f7db31eb4380eb5d316badfe1bb082d9aa57d20f
Ching Chow <ching@getbento.com>
2022-12-24 00:21:56 -0500

    test scanner

--- tests/test_hb.py
+++ tests/test_hb.py
@@ -5,6 +5,7 @@
 
 import hashlib
 import os
+import re
 import tempfile
 import uuid
 import zlib
@@ -43,29 +44,65 @@ def use_stdlib(algo, path):
 
 
 @pytest.fixture
-def randfile():
+def guaranteed():
+    """Supported algos."""
+    return hashlib.algorithms_guaranteed | {"adler32", "crc32"}
+
+
+@pytest.fixture
+def rand_file():
     """Generate a random ~1MB file."""
     temp = tempfile.NamedTemporaryFile(mode="w", delete=False, encoding="utf-8")
     print("\n".join([str(uuid.uuid4()) for _ in range(30000)]), file=temp)
     temp.close()
-    assert os.path.getsize(temp.name) > 1000000
+    assert os.path.getsize(temp.name) > 1_000_000
     yield temp.name
     os.unlink(temp.name)
     assert not os.path.exists(temp.name)
 
 
-def test_compute(subtests, randfile):
+@pytest.fixture
+def rand_digestfile(guaranteed, rand_file):
+    """Generate a file of digests."""
+    temp = tempfile.NamedTemporaryFile(mode="w", delete=False, encoding="utf-8")
+    for algo in guaranteed:
+        print(hb.compute(algo, rand_file), file=temp)
+    temp.close()
+    yield temp.name
+    os.unlink(temp.name)
+    assert not os.path.exists(temp.name)
+
+
+def test_compute(subtests, guaranteed, rand_file):
     """Compare with direct usage of hashlib and zlib."""
-    for algo in hashlib.algorithms_guaranteed | {"adler32", "crc32"}:
+    for algo in guaranteed:
         with subtests.test(algo=algo):
-            using_hb = HashBrown(algo, randfile).compute()
-            using_stdlib = use_stdlib(algo, randfile)
-            assert using_hb == using_stdlib
+            with_hb = HashBrown(algo, rand_file).compute()
+            with_stdlib = use_stdlib(algo, rand_file)
+            assert with_hb == with_stdlib
 
 
-def test_pprint(subtests, randfile):
+def test_pprint(subtests, guaranteed, rand_file):
     """Check pretty print output."""
-    for algo in hashlib.algorithms_guaranteed | {"adler32", "crc32"}:
+    for algo in guaranteed:
         with subtests.test(algo=algo):
-            hexdigest = use_stdlib(algo, randfile)
-            assert hb.compute(algo, randfile) == f"{hexdigest} {algo} {randfile}"
+            with_hb = hb.compute(algo, rand_file)
+            with_stdlib = f"{use_stdlib(algo, rand_file)} {algo} {rand_file}"
+            assert with_hb == with_stdlib
+
+
+def test_scan(rand_digestfile):
+    """Verify file scanner."""
+    with_hb = hb.scan(rand_digestfile, stdout_only=False)
+    with_stdlib = []
+
+    with open(rand_digestfile, encoding="utf-8") as lines:
+        for line in lines:
+            if match := re.match(r"^([0-9A-Fa-f]+) (\w+) (.+)$", line):
+                given, algo, path = match.group(1, 2, 3)
+                if use_stdlib(algo, path) == given:
+                    with_stdlib.append(f"OK {algo} {path}")
+                else:
+                    with_stdlib.append(f"BAD {algo} {path}")
+
+    assert with_hb == with_stdlib
