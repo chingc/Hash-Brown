2b323e8fc4ab961fcfeb314bcb28775f38366991
Ching Chow <ching@getbento.com>
2022-12-28 17:06:02 -0500

    add scanning option

--- hb/__main__.py
+++ hb/__main__.py
@@ -3,8 +3,13 @@ if __name__ == "__main__":
 
     from hb import hb
 
-    for path in sys.argv[2:]:
-        try:
-            print(hb.compute(sys.argv[1], path))
-        except hb.NotAFileError as e:
-            print(e, file=sys.stderr)
+    match sys.argv[1]:
+        case "-c":
+            for path in sys.argv[3:]:
+                try:
+                    print(hb.compute(sys.argv[2], path))
+                except hb.NotAFileError as e:
+                    print(e, file=sys.stderr)
+        case "-s":
+            for path in sys.argv[2:]:
+                hb.scan(path)
